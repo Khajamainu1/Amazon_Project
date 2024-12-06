4e4cbef9b9ffb677a7e86cb18ec28f786311fec3
Khajamainu1 <khaja.mainu1999@gmail.com>
2024-12-06 17:07:28 +0530

    Account Onboard

---
+++ src/test/java/com/automatiom/placeOrder/tests/AccountOnboard.java
@@ -0,0 +1,16 @@
+package com.automatiom.placeOrder.tests;
+
+import org.testng.annotations.Test;
+
+import com.automation.baseTest.BaseTest;
+
+public class AccountOnboard extends BaseTest
+
+{
+
+	@Test
+	public void updateAccInfo()
+	{
+	
+	
+}
